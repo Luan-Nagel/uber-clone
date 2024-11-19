816b784fa15099f1bc975c7693f1d33c65b32697
Luan Nagel Constante <luan.constante@credisulca.coop.br>
2024-11-19 14:47:19 -0300

    project setup: tailwindcss and nativewind

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,9 @@ import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+// Globas CSS file
+import '../../global.css'
+
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
